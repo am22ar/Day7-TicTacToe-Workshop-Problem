436535c512c93e1c0e44cdb49660e2ca6cbb1977
am22ar <amarpatil12222@gmail.com>
2022-10-12 20:21:50 +0530

    Added: ability to choose X or O logic

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -3,7 +3,6 @@ package com.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-
     int i;
     static char[] board = new char[10];
     static char player, computer;
@@ -14,7 +13,18 @@ public class TicTacToe {
             board[i] = ' ';
         }
     }
-
+    public void check()
+    {
+        System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
+        char choice = scan.next().charAt(0);
+        if (choice == 'X') {
+            player = 'X';
+            computer = 'O';
+        } else if (choice == 'Y') {
+            player = 'O';
+            computer = 'X';
+        }
+    }
 	   public static void main(String[] args) {
 		System.out.println("Welcome to TIC_TAC_TOE game");
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
@@ -22,7 +32,8 @@ public class TicTacToe {
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-    
+        //TicTacToe ticTacToe = new TicTacToe();
+        
 		
 	}
 }
