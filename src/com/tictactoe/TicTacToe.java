5bea57b2ca4524378f3c20361aba4eaee9960fb8
am22ar <amarpatil12222@gmail.com>
2022-10-12 20:06:53 +0530

    Added: welcome message for TicTacToe program

---
+++ src/com/tictactoe/TicTacToe.java
@@ -0,0 +1,9 @@
+package com.tictactoe;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+	   public static void main(String[] args) {
+		System.out.println("Welcome to TIC_TAC_TOE game");
+	}
+}
